b54a850095c7d742fbe3996945acb17da0fb9e4e
Ratna Lama <royalray7@gmail.com>
2022-07-11 16:49:08 -0700

    updated docs

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
 # fizzBuzz
+
 A program to write fizzbuzz based on the multiple of a chosen number.
+
+    - if the number is a multiple of 3 print 'fizz'
+    - if the number is a multiple of 5 print 'buzz'
+    - if the number is a multiple of both 3 && 5 print 'fizzbuzz'
+    - for all other cases print the number.
+
+# Input
+
+    - fizzBuzz(num) where num = 15
+
+# Output
+
+    - if num = 1 print 1
+    - if num = 3 print 'fizz'
+    - if num = 5 print 'buzz'
+    - if num = 15 print 'fizzbuzz'
+
+# Clarifications
+
+## 1. Constraints
+
+    - consider cases for only positive integers greater than 0.
+    - limit input/output to prevent stack overflow
+
+## 2. Edge Cases
+
+    - Negative nubmers,
+    - 0,
+    - Null,
+    - Undefined,
+    - Not a Number (NaN),
+    - String,
+    - Argument length,
+    - Overflow,
+    - Underflow
+
+## 3. Assumptions made
+
+    - Consider only the cases for numbers greater than 0.
+
+# Algorithm
+
+Consider following numbers in the number line:
+
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+
+----------------------------------------------------->
+
+Multiples of 3 --> print 'fizz'
+
+- 3x1 = 3
+- 3x2 = 6
+- 3x3 = 9
+- 3x4 = 12 ...
+
+Multiples of 5 --> print 'buzz'
+
+- 5x1 = 5
+- 5x2 = 10
+- 5x4 = 20 ...
+
+Multiples of 3 && 5 --> print 'fizzbuzz'
+
+- 15x1 = 15
+- 15x2 = 30
+- 15x3 = 45 ...
+
+For any other positive numbers just print the same number that are neither multiples of 3 or 5 or both 3 & 5 --> print the same number
+
+Given a number say 3, we can multiply 3 with any positive integers to get a product.
+
+- Say 3x2 = 6 (product)
+- This product 6 can be also called multiple of both 3 and 2 since we obtained it by multiplying 3 by 2.
+- So the real point here is how we know that given a number, it is multiple of 3 or 5 or both?
+- Once we know that we are golden.
+
+- This is where we might have to flip a logic bit. To get a multiple we multiplied. But to know that any given number is a multiple of 3 we simply need to divide it by 3 and if the remainder is 0 then we know it is a multiple.
+
+- In our case 6/2 = 3 remainder is 0, Yes multiple of 3
+- Let's try couple more number 12/3 = 4 remainder is 0, Yes multiple of 3
+- how about 19/3 = 6 remainder is 1 not a multiple of 3
+
+- Therefore we can divide any given number by 3. If the remainder is 0 then it is a multiple of 3
+- Similarly we can divide any given number by 5 and if the remainder is 0 then it is a multiple of 5
+- And if a number is divisible by both 3 && 5 then that number is a multiple of both 3 && 5.
+
+- Next, let's implement our algorithms to code.
+
+# Time Complexity
+
+    - Time O(n) = ?
+    - Space O(n) = ?
+
+# Unit Tests
+
+    - 1. Consider all the following cases:
+    	- Constraints
+    	- Edge cases
+    	- Regular test cases
+    - 2. To run test cases from this repo:
+    	- clone the repo
+    	- then cd to the `fizzBuzz` folder
+    	- install the dependencies `npm install`
+    	- Next, in your terminal window enter following to run all the unit tests.
+    	- `npm test`
+    	- assumes you've `Node` && `Jest` installed in your machine.
+
+# Technologies
+
+- 1.  GitHub
+- 2.  Linkedin
+- 3.  VS Code
+- 4.  Node
+- 5.  NPM Packages
+
+# Kewords
+
+    "JavaScript",
+    "ES6",
+    "JavaScript",
+    "Interview",
+    "Algorithms",
+    "Datastructures",
+    "Interview",
+    "Preparation",
+    "Technical",
+    "Interview"
+
+# References
--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,73 @@
+/**
+ * fizzBuzz.js		- A program that prints the number from 1 to n. Then prints 'fizzbuzz' based on
+ * 					        whether or not a number is a multiple of a certain number.
+ * @author			  - Ratna Lama
+ * @version			  - 1.0.0
+ * @description		- Given a positive integer as an input print:
+ * 					        If a number is a multiple of three print 'fizz'.
+ * 					        If a number is a multiple of five print 'buzz'.
+ * 					        If a number is a multiple of both three and five print 'fizzbuzz'.
+ *
+ * @param {*} num positive integer input
+ * @returns either positive integer || 'fizz' || 'buzz' || 'fizzbuzz'
+ *
+ * Input:
+ * 		fizzBuzz(n) where n = 15
+ * Output:
+ * 		1, 2, (fizz), 4, (buzz), (fizz), 7, 8, (fizz), (buzz), 11, (fizz), 13, 14, fizzbuzz
+ * Constraints/Edge Case:
+ * 		What should the output be if input
+ * 		n = 0 ? || n = -15 || n = 3.75 || n = null || n == undefined || n = string
+ * 		Should we only consider positive integer numbers (n > 0) for this demonstration?
+ * Assumptions if any:
+ * 		Test
+ * Algorithm:
+ * 		 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+ *           ^     ^^ ^        ^  ^^       ^          ^^^
+ * 		Multiples of 3 (^) --> print 'fizz'
+ * 		3x1 = 3
+ * 		3x2 = 6
+ * 		3x3 = 9 ...
+ *
+ * 		Multiples of 5 (^^) --> print 'buzz'
+ * 		5x1 = 5
+ * 		5x2 = 10
+ * 		5x3 = 15 ...
+ *
+ * 		Multiples of 3 && 5 = 15 (^^^) --> print 'fizzbuzz'
+ * 		15x1 = 15
+ * 		15x2 = 30
+ * 		15x3 = 45 ...
+ *
+ * Given a number say 3, we can multiply 3 with any positive integers to get a product.
+ * Say 3x2 = 6 (product)
+ * This product 6 can be called multiple of both  3 & 2since we obtained it by multiplying 3 by 2.
+ * So the real point here is how we know that given a number, it is multiple of 3 or 5 or both?
+ * Once we know that we are golden.
+ *
+ * This is where we might have to flip a logic bit. To get a multiple we multiplied. But to
+ * know that any given number is a multiple of 3 we simply need to divide it by 3 and if the
+ * remainder is 0 then we know it is a multiple.
+ *
+ * In our case 6/2 = 3 remainder is 0, Yes multiple of 3
+ * Let's try couple more number 12/3 = 4 remainder is 0, Yes multiple of 3
+ * how about 19/3 = 6 remainder is 1 not a multiple of 3
+ *
+ * Therefore we can divide any given number by 3. If the remainder is 0 then it is a multiple of 3
+ * Similarly we can divide any given number by 5 and if the remainder is 0 then it is a multiple of 5
+ * And if a number is divisible by both 3 && 5 then that number is a multiple of both 3 && 5.
+ *
+ * Next, let's implement our algorithms to code.
+ *
+ * Time Complexity:
+ * 		- Time O(n) = ?
+ * 		- Space O(n) = ?
+ * Unit Tests:
+ *    - Cosider all the test cases for constraints/edge cases and regular tests
+ */
+
 const fizzBuzzIterative = (num) => {
-  // constraints/edge cases
-  // console.log(` arguments length is: ${arguments.length}`);
-  // console.log(` function length is: ${fizzBuzzIterative.length}`);
   if (typeof num !== 'number' || num < 1) {
-    // if (number < 1 || undefined || null)
     return `Sorry we do not support ${num}. Please try a positive integer number greater than 0.`;
   } else if (num % 3 === 0 && num % 5 === 0) {
     return 'fizzbuzz';
@@ -16,16 +80,10 @@ const fizzBuzzIterative = (num) => {
   }
 };
 
-// console.log(fizzBuzzIterative(undefined));
-
 const fizzBuzzRecursive = (num) => {
   if (typeof num !== 'number' || num < 1) {
     return `Sorry we do not support ${num}. Please try a positive integer number greater than 0.`;
-  }
-  // base case + edge cases
-  //   if (num < 1 || undefined || null) {
-  //     return `Sorry we do not support ${num}. Please try a positive integer number greater than 0.`;
-  else {
+  } else {
     if (num % 3 === 0 && num % 5 === 0) {
       return 'fizzbuzz';
     } else if (num % 3 === 0) {
@@ -38,7 +96,5 @@ const fizzBuzzRecursive = (num) => {
   }
   fizzBuzzRecursive(num + 1);
 };
-
-// console.log(fizzBuzzRecursive('Hello'));
 
 module.exports = { fizzBuzzIterative, fizzBuzzRecursive };
